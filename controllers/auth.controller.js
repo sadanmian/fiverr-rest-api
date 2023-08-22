8855a19d931858cf2b793546ead1e50abe913a41
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-22 22:43:28 +0530

    JWT and Cookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
@@ -22,8 +23,21 @@ export const login = async (req, res) => {
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(400).send("Wrong password or username");
 
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
     const { password, ...info } = user._doc;
-    res.status(200).send(info);
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (error) {
     res.status(500).send("Something went wrong");
   }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,11 @@
-export const fn = (req, res) => {
-  res.send("from controller");
+import User from "../models/user.model.js";
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return res.status(403).send("You can delete only your account!");
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("deleted.");
 };
